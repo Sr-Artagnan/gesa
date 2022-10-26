fa53c349e21c07fba6d0067b60055989e864c0a3
Sr-Artagnan <jonatasrezendearagnan619@gmail.com>
2022-10-25 21:38:48 -0300

    hoje

--- complements/scripts/header_change.js
+++ complements/scripts/header_change.js
@@ -3,7 +3,7 @@
   (function () {
   const header = document.querySelector('#header');
   window.addEventListener('scroll', function () {
-      if (window.scrollY > 150) header.style.cssText = "background-color: #2DC670";
+      if (window.scrollY > 150) header.style.cssText = "background-color: #40bfa2";
       else header.style.cssText = "background-color: transparent";
   });
 })();
